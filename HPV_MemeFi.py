49d47f3a1061d33119ec92d6473a50fc06ec5129
HPV-CLOUD <tgprems444@gmail.com>
2024-09-16 10:53:48 +0500

    Add files via upload

---
+++ HPV_MemeFi.py
@@ -0,0 +1,10 @@
+from HPV_TEAM_MemeFi import HPV_Main
+
+from Core.Config.HPV_Config import *
+
+
+
+if __name__ == '__main__':
+    HPV_Main(MAX_DAMAGE_LVL, MAX_ENERGY_CAP_LVL, MAX_RECHARGING_SPEED_LVL, AUTO_UPDATE)
+
+
